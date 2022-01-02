122b4c4fb2341c6b7ee0190197ae41685d7da98a
AreejAlbaqami <96816565+AreejAlbaqami@users.noreply.github.com>
2022-01-02 03:19:38 +0300

    Add files via upload

--- BookList.swift
+++ BookList.swift
@@ -19,118 +19,39 @@ func booklist(){
     if b == "yes" {
         menu()}
              switch b {
-                 //______________________________________________________
+              
                  //NOVEL
             case "1":
                 //BOOK NOVEL
-                for i in booksnovel{
-                    print(i)}
-                //BOOK INFORMATIONS
-               print("-Choose a book by entering on of the capital letter:")
-                let m = Tils.readString()
-                 if m == "A"{
-                    booknovel.booknames()
-                     booknovel.available()}
-                if m  == "B"{
-                    booknovel1.booknames()
-                    booknovel1.available()}
-                if m  == "C"{
-                    booknovel2.booknames()
-                    booknovel2.available()}
-                 else {
-                print("-there is no section or book. Check how you writing of the letter.")
-                     booklist()}
-                   
-                 //______________________________________________________
+                 novel()
+              
                 //PSYCHOLOGY
-                
             case "2":
                 //BOOK PSYCHOLOGY
-                for i in bookspsychology{
-                    print(i)}
-                //BOOK INFORMATIONS
-                print("-Choose a book by entering on of the capital letter")
-                let m = Tils.readString()
-                if m  == "A"{
-                    bookpsychology.booknames()
-                    bookpsychology.available()
-                }
-                 if m  == "B"{
-                    bookpsychology1.booknames()
-                    bookpsychology1.available()}
-                 else {
-                print("-there is no section or book. Check how you writing of the letter.")
-                     booklist()}
+                psychology()
 
-               //______________________________________________________
               //COMPUTER
-                
             case "3":
                 //BOOK COMPUTER
-                for i in bookscomputer{
-                    print(i)}
-                //BOOK INFORMATIONS
-                print("-Choose a book by entering on of the capital letter")
-                let m = Tils.readString()
-                 if m  == "A"{
-                     bookcomputer.booknames()
-                     bookcomputer.available()}
-                 if m  == "B"{
-                    bookcomputer1.booknames()
-                    bookcomputer1.available()}
-            
-                 else {
-                print("-there is no section or book. Check how you writing of the letter.")
-                     booklist()}
-
-              //______________________________________________________
-             // HISTORY
+                computer()
+             
+            // HISTORY
              case "4":
                 //BOOK HISTORY
-                for i in bookshistory{
-                     print(i)}
-                //BOOK INFORMATIONS
-                print("-Choose a book by entering on of the capital letter")
-                let m = Tils.readString()
-                 if m  == "A"{
-                     bookhistory.booknames()
-                     bookhistory.available()}
-                 else {
-                print("-there is no section or book. Check how you writing of the letter.")
-                     booklist()}
-                //______________________________________________________
+                 history()
+
                 //STORY
             case "5":
                 // BOOK STORY
-                for i in booksstory{
-                    print(i)}
-                //BOOK INFORMATIONS
-                print("-Choose a book by entering on of the capital letter")
-                let m = Tils.readString()
-                 if m  == "A"{
-                     bookstory.booknames()
-                     bookstory.available()}
-                 else {
-                print("-there is no section or book. Check how you writing of the letter.")
-                     booklist()}
-                //______________________________________________________
-                // LITERATURE
+                story()
+                
+                 //LITERATURE
             case "6":
-                //  BOOK LITERATURE
-                for i in booksstory{
-                    print(i)}
-                // BOOK INFORMATIONS
-                 print("-Choose a book by entering on of the capital letter")
-                let m = Tils.readString()
-                if m  == "A"{
-                    bookliterature.booknames()
-                    bookliterature.available()}
-                 else {
-                 print("-there is no section or book. Check how you writing of the letter.")
-                     booklist()}
+                // BOOK LITERATURE
+              literature()
             default:
               
-                print("-there is no section or book. Check how you writing of the letter.")
+                print("-there is no section")
                      booklist()
             }
 
